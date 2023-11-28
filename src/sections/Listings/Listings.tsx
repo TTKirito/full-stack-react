7b22881e06a2c15b52dd03f415fa65ce83722f39
Thuận Tôn <137130109+ThuanTon98@users.noreply.github.com>
2023-11-28 18:19:02 +0700

    21. fetch data api

--- src/sections/Listings/Listings.tsx
+++ src/sections/Listings/Listings.tsx
@@ -1,11 +1,36 @@
 import React, { FunctionComponent } from "react";
+import { server } from "../../lib/api";
+
+const LISTINGS = `
+    query Listings {
+        listings {
+            _id
+            title
+            image
+            address
+            price
+            numOfGuests
+            numOfBeds
+            rating
+        }
+    }
+`
 
 interface Props {
     title: string;
 }
 
 export const Listings = ({ title }: Props) => {
-    return <h2>{title}</h2>
+    const fetchListings = async () => {
+        const listings = await server.fetch({ query: LISTINGS });
+        console.log(listings, 'hixxxxxxxxxxxxxxx')
+    }
+    return (
+        <div>
+            <h2>{title}</h2>
+            <button onClick={fetchListings}>Query!</button>
+        </div>
+    )
 }
 
 // export const Listings2: FunctionComponent<Props> = ({ title }) => {
