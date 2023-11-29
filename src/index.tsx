d7541d14af4612d9bbce2b4fbc6f9f6c59a7f59d
Thuận Tôn <137130109+ThuanTon98@users.noreply.github.com>
2023-11-29 16:34:57 +0700

    31. Apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,19 @@ import React from "react";
 import { render } from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import Listings from "./sections/Listings";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-render(<Listings title="Listing" />, document.getElementById("root"));
+const client = new ApolloClient({
+  uri: "http://localhost:9000/api",
+  cache: new InMemoryCache(),
+});
+
+render(
+  <ApolloProvider client={client}>
+    <Listings title="Thuan"></Listings>
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
