624acc95855380f783c0a518536ec913109545af
Thuận Tôn <137130109+ThuanTon98@users.noreply.github.com>
2023-12-15 10:31:13 +0700

    97: not found

--- src/sections/NotFound/index.tsx
+++ src/sections/NotFound/index.tsx
@@ -1,3 +1,28 @@
+import { Button, Empty, Layout, Typography } from "antd";
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+const { Content } = Layout;
+const { Text } = Typography;
+
 export const NotFound = () => {
-  return <h2>NotFound</h2>;
+  return (
+    <Content className="not-found">
+      <Empty
+        description={
+          <Fragment>
+            <Text className="not-found__description-title">
+              Uh oh! Something went wrong
+            </Text>
+            <Text className="not-found__description-subtitle">
+              The page you're looking for can't be found
+            </Text>
+          </Fragment>
+        }
+      />
+      <Link to="/" className="not-found_cta ant-btn ant-btn-primary ant-btn-lg">
+        <Button>Go to Home</Button>
+      </Link>
+    </Content>
+  );
 };
